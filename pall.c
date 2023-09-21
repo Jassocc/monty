6e21a05d6fc0d9fbd2b1de4e240591d273e061de
Jassocc <clnearle@gmail.com>
2023-09-21 12:53:52 -0700

    pall original

--- pall.c
+++ pall.c
@@ -9,24 +9,9 @@ void pall(stack_t **h, unsigned int line_number)
 	stack_t *temp = *h;
 
 	(void) line_number;
-	if (mode == 0)
+	while (temp)
 	{
-		while (temp)
-		{
-			printf("%d\n", temp->n);
-			temp = temp->next;
-		}
-	}
-	else if (mode == 1)
-	{
-		while (temp->next)
-		{
-			temp = temp->next;
-		}
-		while (temp)
-		{
-			printf("%d\n", temp->n);
-			temp = temp->prev;
-		}
+		printf("%d\n", temp->n);
+		temp = temp->next;
 	}
 }
